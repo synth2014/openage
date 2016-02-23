cb725a61c8caf92c90de96633a8e849f7b3bf6dd
synth2014 <abhijit.nandy@gmail.com>
2016-02-24 00:48:07 +0530

    Remving linux macro for branch prediction as suggested at http://stackoverflow.com/questions/1440570/likely-unlikely-equivalent-for-msvc

--- libopenage/util/compiler.h
+++ libopenage/util/compiler.h
@@ -18,8 +18,13 @@
  *
  * btw, this implementation was taken from the Linux kernel.
  */
-#define likely(x)    __builtin_expect(!!(x), 1)
-#define unlikely(x)  __builtin_expect(!!(x), 0)
+#ifdef _MSC_VER
+    #define likely(x)    (x)
+    #define unlikely(x)  (x)
+#elif
+    #define likely(x)    __builtin_expect(!!(x), 1)
+    #define unlikely(x)  __builtin_expect(!!(x), 0)
+#endif
 
 namespace openage {
 namespace util {
